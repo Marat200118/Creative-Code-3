3b13c9cd92b4eea309120381d41fee805d57e47e
Marat200118 <marat.samigullin1@gmail.com>
2023-10-12 22:19:29 +0200

    adding data fixed

--- Pose-alarm/js/script.js
+++ Pose-alarm/js/script.js
@@ -1,10 +1,11 @@
 let knnClassifier;
-let inputs;
 let video;
 let canvas;
 let ctx;
 let poseNet;
-let currentExercise = ""; // Track the current exercise (either "squats" or "")
+let squatCount = 0;
+let lastPose = 0;
+const exerciseLabel = "squat down";
 
 const keypointNames = [
   "leftShoulder",
@@ -16,7 +17,15 @@ const keypointNames = [
   "nose",
   "leftEye",
   "rightEye",
+  "leftHip",
+  "rightHip",
+  "leftKnee",
+  "rightKnee",
+  "leftAnkle",
+  "rightAnkle",
 ];
+
+let pose;
 
 const init = async () => {
   knnClassifier = ml5.KNNClassifier();
@@ -30,83 +39,138 @@ const init = async () => {
   video.play();
 
   canvas = document.querySelector(".canvas");
-  canvas.width = video.width;
-  canvas.height = video.height;
+  video.addEventListener("loadedmetadata", function () {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+  });
   ctx = canvas.getContext("2d");
 
   poseNet = ml5.poseNet(video, modelReady);
   poseNet.on("pose", resultHandler);
 
+  updateCountDisplay();
+
   const $addDataButton = document.querySelector(".addData");
-  $addDataButton.addEventListener("click", addDataHandler);
+  $addDataButton.addEventListener("click", addSquatDataHandler);
 
-  const $classifyButton = document.querySelector(".classify");
-  $classifyButton.addEventListener("click", classifyHandler);
+  const $resetSquatCountButton = document.querySelector(".resetSquatCount");
+  $resetSquatCountButton.addEventListener("click", resetSquatCount);
+
+  const $classifySquatButton = document.querySelector(".classifySquat");
+  $classifySquatButton.addEventListener("click", classifySquatHandler);
 };
 
-const classifyHandler = () => {
-  knnClassifier.classify(inputs, classifyResultHandler);
+const updateCountDisplay = () => {
+  document.querySelector(".status").textContent = `Squats: ${squatCount}`;
 };
 
-const classifyResultHandler = (error, result) => {
-  if (error) return console.error(error);
-  const exerciseLabel = result.label;
+const classifySquatHandler = () => {
+  classifySquatPosition();
+};
 
-  if (exerciseLabel === "squats") {
-    if (currentExercise !== "squats") {
-      currentExercise = "squats";
-      console.log("Squats started.");
-    } else {
-      console.log("Squat counted.");
+const classifySquatPosition = () => {
+    // if (pose) {
+    //   const numExamples = knnClassifier.getNumExamples();
+    //   if (numExamples <= 0) {
+    //     console.log("No examples in any label");
+    //     return;
+    //   }
+    // }
+  
+  if (pose) {
+    const inputs = keypointNames.map((part) =>
+      pose[part] ? [pose[part].x, pose[part].y] : [0, 0]
+    );
+
+    knnClassifier.classify(inputs, (error, result) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      // console.log("Classification result:", result);
+
+      const squatLabel = result.label;
+
+      if (squatLabel === exerciseLabel) {
+        if (lastPose === 1) {
+          squatCount++;
+          lastPose = 0;
+          updateCountDisplay();
+        }
+      } else {
+        lastPose = 1;
+      }
+    });
+  }
+};
+
+const addSquatDataHandler = () => {
+  console.log("Adding data for label:", exerciseLabel);
+  if (!pose) {
+    console.log("No pose detected.");
+    return;
+  }
+  if (pose) {
+    const exerciseLabelInput = document.querySelector(".label");
+    const inputLabel = exerciseLabelInput.value;
+    const inputs = keypointNames.map((part) =>
+      pose[part] ? [pose[part].x, pose[part].y] : [0, 0]
+    );
+
+    console.log(
+      `Added data for label: ${exerciseLabel}, with keypoints:`,
+      inputs
+    );
+
+    knnClassifier.addExample(inputs, exerciseLabel);
+
+    const dataset = knnClassifier.getClassifierDataset();
+    if (dataset) {
+      Object.keys(dataset).forEach((key) => {
+        console.log(`Label: ${key}`);
+        const data = dataset[key].dataSync();
+        console.log("Data:", Array.from(data));
+      });
     }
-  }
 
-  document.querySelector(
-    ".status"
-  ).textContent = `Current Exercise: ${currentExercise}`;
+    const notification = document.querySelector(".notification");
+    notification.textContent = `Data added for ${exerciseLabel}!`;
+    setTimeout(() => {
+      notification.textContent = ""; // Clear the message after 3 seconds
+    }, 3000);
+  }
 };
 
-const addDataHandler = () => {
-  const $label = document.querySelector(".label");
-  const output = $label.value;
-
-  if (output === "pushups" || output === "squats") {
-    knnClassifier.addExample(inputs, output);
-  } else {
-    console.log('Invalid exercise label. Please enter "pushups" or "squats".');
-  }
+const resetSquatCount = () => {
+  squatCount = 0;
+  updateCountDisplay();
 };
 
 const resultHandler = (poses) => {
   if (!poses.length) return;
 
-  const pose = poses[0].pose;
+  pose = poses[0].pose;
   const keypoints = pose.keypoints;
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
   ctx.fillStyle = "red";
   ctx.strokeStyle = "white";
   ctx.lineWidth = 2;
 
   for (const keypoint of keypoints) {
-    const keyPointNeedsTobeRendered = keypointNames.includes(keypoint.part);
-    if (keyPointNeedsTobeRendered) {
+    const keyPointNeedsToBeRendered = keypointNames.includes(keypoint.part);
+    if (keyPointNeedsToBeRendered) {
       const { x, y } = keypoint.position;
-      if (keypoint.part === "nose") {
-        // console.log(x, y);
-      }
       ctx.beginPath();
       ctx.arc(x, y, 5, 0, 2 * Math.PI);
       ctx.fill();
       ctx.stroke();
     }
   }
-
-  inputs = keypoints
-    .filter((kp) => keypointNames.includes(kp.part))
-    .map((keypoint) => [keypoint.position.x, keypoint.position.y]);
 };
+
+
 
 const modelReady = () => {
   console.log("PoseNet model loaded");
